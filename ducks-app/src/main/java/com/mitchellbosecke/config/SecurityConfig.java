d24bea4ba9ebd571ac3b08535d578425c33c303b
Mitchell Bosecke <mitchellbosecke@gmail.com>
2021-10-09 14:53:28 -0600

    Added explicit schema name to user lookup query to hopefully solve a Heroku-specific problem where it can't find the "users" table during login.

--- ducks-app/src/main/java/com/mitchellbosecke/config/SecurityConfig.java
+++ ducks-app/src/main/java/com/mitchellbosecke/config/SecurityConfig.java
@@ -6,11 +6,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import javax.sql.DataSource;
@@ -36,7 +33,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource);
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,enabled from ducks.users where username = ?")
+                .authoritiesByUsernameQuery("select username,authority from ducks.authorities where username = ?");
     }
 
     @Bean
