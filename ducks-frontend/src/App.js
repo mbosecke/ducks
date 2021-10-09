58dad2055eb886eaab338690ad9031bc5c100f91
Mitchell Bosecke <mitchellbosecke@gmail.com>
2021-10-09 10:16:18 -0600

    Removed unused "react-query" library.

--- ducks-frontend/src/App.js
+++ ducks-frontend/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import {ChakraProvider} from "@chakra-ui/react";
 import {BrowserRouter} from "react-router-dom";
-import {QueryClient, QueryClientProvider} from 'react-query';
 import ducksTheme from "./theme/ducksTheme";
 
 import PrimaryRouter from "./PrimaryRouter";
-
-// React Query
-const queryClient = new QueryClient();
 
 function App() {
 
     return (
         <>
             <ChakraProvider theme={ducksTheme}>
-                <QueryClientProvider client={queryClient}>
-                    <BrowserRouter>
-                        <PrimaryRouter/>
-                    </BrowserRouter>
-                </QueryClientProvider>
+                <BrowserRouter>
+                    <PrimaryRouter/>
+                </BrowserRouter>
             </ChakraProvider>
         </>
     );
